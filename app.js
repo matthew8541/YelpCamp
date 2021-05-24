b871b999022ea21d02d3f799404a5296962a7cb4
matthew8541 <ytseng35@gatech.edu>
2021-05-24 23:45:58 +0800

    Campground model created

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const path = require("path");
+
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection errer"));
+db.once("open", () => {
+  console.log("Database Connected!");
+})
+
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello FROM YELP CAMP");
+  res.render("Home");
+})
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({title: "My Backyard", description: "cheap camping!"});
+  await camp.save();
+  res.send(camp);
 })
 
 app.listen(3000, () => {
