ce4e2b5bc761a952755a61ad4798f49d1228d70b
matthew8541 <ytseng35@gatech.edu>
2021-06-01 21:42:21 +0800

    authentication added

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 // custom modules
-const Campground = require("./models/campground");
-const Review = require("./models/review");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema, reviewSchema } = require("./schemas");
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const User = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -53,17 +53,26 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // setup flash middleware
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash("error");
   next()
 })
 
 // router
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/", userRoutes);
+
 
 app.get("/", (req, res) => {
   res.render("Home");
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require("../utils/catchAsync");
 const ExpressError = require('../utils/ExpressError');
 const Campground = require("../models/campground");
 const { campgroundSchema } = require("../schemas");
+const {isLoggedIn} = require("../middleware");
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -23,13 +24,13 @@ router.get("/", async (req, res) => {
 
 // Needs to be placed before "/campgrounds/:id"
 // Otherwise, the program will treat new as :id
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
   res.render("campgrounds/new");
 })
 
 // catchAsync: custom error function under utils folder
 // Add a middleware: validateCampground
-router.post("/", validateCampground, catchAsync(async (req, res, next) => {
+router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
   // 400 -> Client Error
   // if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400)
   const campground = new Campground(req.body.campground);
@@ -47,7 +48,7 @@ router.get("/:id", catchAsync(async (req, res) => {
   res.render("campgrounds/show", { campground });
 }))
 
-router.get("/:id/edit", catchAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   if (!campground) {
     req.flash("error", "Cannot find that campground!");
@@ -56,14 +57,14 @@ router.get("/:id/edit", catchAsync(async (req, res) => {
   res.render("campgrounds/edit", { campground });
 }))
 
-router.put("/:id", validateCampground, catchAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   req.flash("success", "Successfully updated campground");
   res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete("/:id", catchAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds");
